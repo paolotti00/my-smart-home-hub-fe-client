0abea3e99f5667b3635752d56f947ca287535185
paolotti00 <paolo.tripodi@aubay.it>
2022-12-10 01:09:15 +0100

    rooms

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RoomsListComponent } from './components/rooms-list/rooms-list.component';
+import { RoomItemComponent } from './components/room-item/room-item.component';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
 
 
 @NgModule({
   declarations: [
-    RoomsListComponent
+    RoomsListComponent,
+    RoomItemComponent
   ],
   exports: [
     RoomsListComponent
   ],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        FontAwesomeModule
+    ]
 })
 export class SharedModule { }
