09abf3e8d63c988517eeb027d4a5b8b04560e1cb
Paolo Tripodi <paolo.tripodi@enel.com>
2022-12-30 18:44:42 +0100

    fixed some ui errors

--- src/app/shared/components/device/device-item/device-item.component.ts
+++ src/app/shared/components/device/device-item/device-item.component.ts
@@ -1,6 +1,5 @@
 import {Component, HostBinding, Input} from '@angular/core';
 import {DeviceModel} from "../../../../core/models/device.model";
-import {ComponentTypeEnum} from "../../../../core/enums/componentType.enum";
 
 @Component({
   selector: 'app-device-item',
@@ -9,17 +8,17 @@ import {ComponentTypeEnum} from "../../../../core/enums/componentType.enum";
 })
 export class DeviceItemComponent {
   @Input()
-  inListMode:boolean=false; // pass as true if you are using this component inside the device-list component
+  inListMode: boolean = false; // pass as true if you are using this component inside the device-list component
   @Input()
   device = {} as DeviceModel
   @Input()
-  colSpan:number=1
+  colSpan: number = 1
+
   @HostBinding('class')
-  public get getClass():string {
-    return 'col-span-'+this.colSpan;
+  public get getClasses(): string {
+    return 'col-span-' + this.colSpan
+      + ' ' + 'bg-lime-100' // backgourd color
+      + ' ' + 'h-fit' // height
+      + ' ' + 'p-2'; // padding
   }
-  componentType_MIX = ComponentTypeEnum.MIX;
-  componentType_LIGHT = ComponentTypeEnum.LIGHT;
-  componentType_HEAT = ComponentTypeEnum.SENSOR_TEMPERATURE;
-  componentType_HUMIDITY = ComponentTypeEnum.SENSOR_HUMIDITY;
 }
