5dcca4bea7274d77ce89d1886b41a34462a303bf
Paolo Tripodi <paolo.tripodi@enel.com>
2023-03-17 16:11:41 +0100

    docker, fixed build problem

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  SERVER_API_URL:"http://raspberrypi.local:8080"
+  SERVER_API_URL:"http://raspberrypi.local:8080",
+  SERVER_MOCKS_API_URL:"http://192.168.50.170:3001"
 };
